b59ce98d90771d06c3eb7c34a457fe6a9cff3857
Senthilkumar T <76416166+thirusk86@users.noreply.github.com>
2021-07-16 14:51:17 +0800

    Change nixos-rebuild path to current system

--- modules/module.nix
+++ modules/module.nix
@@ -10,29 +10,28 @@ with lib;
       type = package;
       default = pkgs.git;
     };
-    nixFlakesPackage = mkOption {
-      type = package;
-      default = pkgs.nixFlakes;
-    };
-    nixRebuildPackage = mkOption {
-      type = package;
-      default = pkgs.nixos-rebuild;
-    };	
+    #nixFlakesPackage = mkOption {
+      #type = package;
+      #default = pkgs.nixFlakes;
+    #};
+    #nixRebuildPackage = mkOption {
+      #type = package;
+      #default = pkgs.nixos-rebuild;
+    #};	
   };
 
   config =
     let
       cfg = config.services.nixos-auto-update;
       gitPath = "${cfg.gitPackage}/bin/git";
-      nixFlakesPath = "${cfg.nixFlakesPackage}/bin/nix";
-      nixRebuildPath = "${cfg.nixRebuildPackage}/bin/nixos-rebuild";
+      #nixFlakesPath = "${cfg.nixFlakesPackage}/bin/nix";
+      #nixRebuildPath = "${cfg.nixRebuildPackage}/bin/nixos-rebuild";
       mkStartScript = name: pkgs.writeShellScript "${name}.sh" ''
         set -euo pipefail
         PATH=${makeBinPath (with pkgs; [ git nix ])}
         cd /etc/nixos/
-        ${gitPath} pull origin master
-	${nixFlakesPath} flake update '/etc/nixos/' -v
-	${nixRebuildPath} switch --flake '/etc/nixos/#nixtst' --impure	
+        ${gitPath} pull origin master	
+	/run/current-system/sw/bin/nixos-rebuild switch --flake '/etc/nixos/#nixtst' --impure	
       '';
     in
       mkIf cfg.enable (
